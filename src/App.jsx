d04056e96a305c341443a492e3b6dbd15894dcfc
swatikolte2000 <swatikolte2000@gmail.com>
2024-07-22 17:02:05 +0530

    subheadingchanges

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <h2>React git project</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
