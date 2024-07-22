20b55d0cc4c4c1d0476dab41483313edc6180974
swatikolte2000 <swatikolte2000@gmail.com>
2024-07-22 17:12:19 +0530

    forexample

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>React git project</h2>
+      <h3>Boilerplate code practise</h3>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
